9c9149d6fe38eb7e02f82789c20a18cd60eda0ae
Mochan <mochancrimthann@users.noreply.github.com>
2022-05-26 07:10:23 +0000

    uncomment dialogue

--- src/events/ending.js
+++ src/events/ending.js
@@ -31,102 +31,102 @@ export async function execute(world, eventId) {
 	}
 	const player = entities[0]
 
-	// await timedText(
-	// 	3 * 1000,
-	// 	world,
-	// 	Position.x[eventId] - 64,
-	// 	Position.y[eventId] - 64,
-	// 	128,
-	// 	128,
-	// 	'HI BEEF! What are you doing out here?'
-	// )
+	await timedText(
+		3 * 1000,
+		world,
+		Position.x[eventId] - 64,
+		Position.y[eventId] - 64,
+		128,
+		128,
+		'HI BEEF! What are you doing out here?'
+	)
 
-	// await timedText(
-	// 	3.5 * 1000,
-	// 	world,
-	// 	Position.x[eventId] - 64,
-	// 	Position.y[eventId] - 64,
-	// 	128,
-	// 	128,
-	// 	'Did you came all this way to find me?'
-	// )
+	await timedText(
+		3.5 * 1000,
+		world,
+		Position.x[eventId] - 64,
+		Position.y[eventId] - 64,
+		128,
+		128,
+		'Did you came all this way to find me?'
+	)
 
-	// await timedText(
-	// 	4 * 1000,
-	// 	world,
-	// 	Position.x[eventId] - 64,
-	// 	Position.y[eventId] - 64,
-	// 	128,
-	// 	128,
-	// 	'Wow!! That\'s so impressive! Good job Beef!'
-	// )
+	await timedText(
+		4 * 1000,
+		world,
+		Position.x[eventId] - 64,
+		Position.y[eventId] - 64,
+		128,
+		128,
+		'Wow!! That\'s so impressive! Good job Beef!'
+	)
 
-	// const coins = Purse.value[player]
-	// if (coins > 0) {
-	// 	if (coins === 1) {
-	// 		await timedText(
-	// 			4 * 1000,
-	// 			world,
-	// 			Position.x[eventId] - 64,
-	// 			Position.y[eventId] - 64,
-	// 			128,
-	// 			128,
-	// 			'You even found a coin!'
-	// 		)
-	// 	} else if (coins > 1) {
-	// 		await timedText(
-	// 			4 * 1000,
-	// 			world,
-	// 			Position.x[eventId] - 64,
-	// 			Position.y[eventId] - 64,
-	// 			128,
-	// 			128,
-	// 			'Wait... you even found some coins?'
-	// 		)
+	const coins = Purse.value[player]
+	if (coins > 0) {
+		if (coins === 1) {
+			await timedText(
+				4 * 1000,
+				world,
+				Position.x[eventId] - 64,
+				Position.y[eventId] - 64,
+				128,
+				128,
+				'You even found a coin!'
+			)
+		} else if (coins > 1) {
+			await timedText(
+				4 * 1000,
+				world,
+				Position.x[eventId] - 64,
+				Position.y[eventId] - 64,
+				128,
+				128,
+				'Wait... you even found some coins?'
+			)
 
-	// 		await timedText(
-	// 			4 * 1000,
-	// 			world,
-	// 			Position.x[eventId] - 64,
-	// 			Position.y[eventId] - 64,
-	// 			128,
-	// 			128,
-	// 			`You found ${Purse.value[player]} of them!!`
-	// 		)
+			await timedText(
+				4 * 1000,
+				world,
+				Position.x[eventId] - 64,
+				Position.y[eventId] - 64,
+				128,
+				128,
+				`You found ${Purse.value[player]} of them!!`
+			)
 
-	// 		if (coins > 20) {
-	// 			await timedText(
-	// 				4 * 1000,
-	// 				world,
-	// 				Position.x[eventId] - 64,
-	// 				Position.y[eventId] - 64,
-	// 				128,
-	// 				128,
-	// 				'How did you even carry all these?'
-	// 			)
-	// 		}
-	// 	}
-	// }
+			if (coins > 20) {
+				await timedText(
+					4 * 1000,
+					world,
+					Position.x[eventId] - 64,
+					Position.y[eventId] - 64,
+					128,
+					128,
+					'How did you even carry all these?'
+				)
+			}
+		}
+	}
 
-	// await timedText(
-	// 	3 * 1000,
-	// 	world,
-	// 	Position.x[eventId] - 64,
-	// 	Position.y[eventId] - 64,
-	// 	128,
-	// 	128,
-	// 	'By the way... it looks like you came the long way.'
-	// )
+	await timedText(
+		3 * 1000,
+		world,
+		Position.x[eventId] - 64,
+		Position.y[eventId] - 64,
+		128,
+		128,
+		'By the way... it looks like you came the long way.'
+	)
 
-	// await timedText(
-	// 	4 * 1000,
-	// 	world,
-	// 	Position.x[eventId] - 64,
-	// 	Position.y[eventId] - 72,
-	// 	128,
-	// 	128,
-	// 	'You know this beach is just to the west of our house, right?'
-	// )
+	await timedText(
+		4 * 1000,
+		world,
+		Position.x[eventId] - 64,
+		Position.y[eventId] - 72,
+		128,
+		128,
+		'You know this beach is just to the west of our house, right?'
+	)
 
 	await timedText(
 		4 * 1000,
@@ -144,7 +144,6 @@ export async function execute(world, eventId) {
 	const canvas = world.canvas.c
 	const x = world.bounds.x - (canvas.width / 2)
 	const y = world.bounds.y + (canvas.height / 2)
-	console.log(x, y)
 
 	await timedText(
 		4 * 1000,
